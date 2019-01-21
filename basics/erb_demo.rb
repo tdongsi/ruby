9331f5e43766105b4f7e3087458d96652bd3e77a
tdongsi <dongsi.tuecuong@gmail.com>
2019-01-20 22:50:50 -0800

    Template from file.

--- basics/erb_demo.rb
+++ basics/erb_demo.rb
@@ -6,8 +6,10 @@ ntp_mapping = {
     'london' => 'uk.pool.ntp.org',
     'nyc' => 'us.pool.ntp.org'
 }
+
 ntp_local_server = '192.138.0.1'
 ntp_regional_server = ntp_mapping[ntp_location]
+monitor = false
 
 template = ERB.new <<-EOF
 # Managed by Puppet - do not edit
@@ -16,4 +18,9 @@ server <%= ntp_regional_server %>
 driftfile /var/lib/ntp/drift
 EOF
 
+file = File.read('ntp.conf.erb')
+template2 = ERB.new(file)
+
 puts template.result(binding)
+puts
+puts template2.result(binding)
